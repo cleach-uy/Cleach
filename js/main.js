25c4987d3072436216e69ee6edbed1a635c2a76e
Ariel Hernandez <ariel.dox@gmail.com>
2023-10-10 22:43:26 -0300

    Update contact form

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
 
         $.ajax({
             type: 'POST',
-            url: $(this).attr('action'),
+            url: 'https://formspree.io/f/xvojdgqa',
             data: formData,
+            dataType: 'json',  // Asegúrate de especificar el tipo de dato que esperas de la respuesta
             success: function(response) {
                 alert('¡El formulario se ha enviado con éxito!');
-                
             },
             error: function(error) {
                 alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
