0677503a3106e8aae449eb9d0b754489f3b668bf
Ariel Hernandez <ariel.dox@gmail.com>
2023-10-10 22:13:09 -0300

    Update contact form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('#contactForm').submit(function(e) {
-        e.preventDefault(); // Prevenir el envío normal del formulario
+        e.preventDefault();
 
-        var formData = $(this).serialize(); // Obtener los datos del formulario
+        var formData = $(this).serialize();
 
         $.ajax({
             type: 'POST',
@@ -10,7 +10,7 @@ $(document).ready(function() {
             data: formData,
             success: function(response) {
                 alert('¡El formulario se ha enviado con éxito!');
-                // Aquí puedes agregar cualquier otro código que desees ejecutar después de enviar el formulario
+                
             },
             error: function(error) {
                 alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
