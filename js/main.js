6b18f792d65c82060d706d871c479ffec06f0aeb
arielhernandezcl <ariel.dox@gmail.com>
2023-11-27 12:22:15 -0300

    Update event calendar

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', async function() {
             card.innerHTML = `
                         <div id="card-item" class="card">
                             <div class="card-body">
-                            <h5 class="card-title" style="font-size: 1.2rem; font-weight: bold;">${obtenerDiaMesCreativo(evento.fecha)}</h5>
+                            <h5 class="card-title" style="font-size: 1.2rem; font-weight: bold; color:#105C79;">${obtenerDiaMesCreativo(evento.fecha)}</h5>
                                 <h6 class="card-title" style="font-size: 1.1rem; text-decoration: underline;">${evento.nombreEvento}</h6>
                                 <p class="card-text" style="font-size: 14px;">Lugar: ${evento.lugar}</p>
                                 <p class="card-text" style="font-size: 14px;"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg> Hora: ${evento.hora}</p>
