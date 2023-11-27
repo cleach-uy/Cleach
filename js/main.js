60ee07adb41cd1dfe8dccaf8f69c330283523667
arielhernandezcl <ariel.dox@gmail.com>
2023-11-27 03:36:33 -0300

    Update video home and add event api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $('#contactForm').submit(function(e) {
         e.preventDefault();
@@ -63,32 +64,3 @@ document.addEventListener('DOMContentLoaded', async function() {
 });
 
 // EVENT DATA
-
-const options = {
-    fullscreen: {
-      enabled: true,
-      iosNative: false
-    },
-    controls: [
-      "play-large",
-      "play",
-      "progress",
-      "duration",
-      "mute",
-      "volume",
-      "fullscreen"
-    ],
-    muted: true,
-    storage: { enabled: false }
-  };
-  
-  const optionsIOS = {
-    ...options,
-    fullscreen: {
-      enabled: true,
-      iosNative: true
-    }
-  };
-  const player = new Plyr(document.querySelector(".player"), options);
-  const playerIOS = new Plyr(document.querySelector(".player-ios"), optionsIOS);
-  
